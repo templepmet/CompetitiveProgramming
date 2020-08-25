90176d58acb1965a932fe20b0ba81a950f2c9308
templepmet <templepmet@gmail.com>
2020-08-25 10:35:36 +0900

    any update

---
+++ Others/test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF (1 << 30)
+#define EPS 1e-10
+#define MOD 1000000007
+
+#define rep(i, n) FOR(i, 0, n)
+#define FOR(i, x, n) for (int i = (x); i < (n); ++i)
+#define all(v) (v).begin(), (v).end()
+
+using ll = long long;
+
+template<class T> void chmax(T& a, T b){ a>b?:a=b; }
+template<class T> void chmin(T& a, T b){ a<b?:a=b; }
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << i + 1 << endl;
+    }
+
+    return 0;
+}
