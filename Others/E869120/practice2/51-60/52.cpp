29fec6ca6d03125a79848e9311e82da0409ba592
templepmet <templepmet@gmail.com>
2020-09-22 02:10:00 +0900

    add -53

--- Others/E869120/practice2/51-60/52.cpp
+++ Others/E869120/practice2/51-60/52.cpp
@@ -15,8 +15,41 @@ using ll = long long;
 template<class T> bool chmax(T& a, T b){ if (a < b) { a = b; return true; } return false; }
 template<class T> bool chmin(T& a, T b){ if (a > b) { a = b; return true; } return false; }
 
+int n, m;
+int a[100000];
+int b[20][100001];
+int c[20];
+int dp[1 << 20];
+
 int main() {
-    
+    cin >> n >> m;
+    rep(i, n) {
+        cin >> a[i];
+        a[i]--;
+        b[a[i]][i + 1] = 1;
+        c[a[i]]++;
+    }
+    rep(i, m) {
+        rep(j, n) {
+            b[i][j + 1] += b[i][j];
+        }
+    }
+    fill(dp, dp + (1 << m), INF);
+    dp[0] = 0;
+    for (int s = 0; s < (1 << m); ++s) {
+        int l = 0;
+        for (int i = 0; i < m; ++i) {
+            if (s & (1 << i)) {
+                l += c[i];
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            if ((s & (1 << i)) == 0) {
+                chmin(dp[s | (1 << i)], dp[s] + (c[i] - (b[i][l + c[i]] - b[i][l])));
+            }
+        }
+    }
+    cout << dp[(1 << m) - 1] << endl;
 
     return 0;
 }
