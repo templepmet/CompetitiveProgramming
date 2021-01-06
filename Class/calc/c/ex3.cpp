87c250d7be3156aa491050b7c03be98716e34dd1
templepmet <templepmet@gmail.com>
2021-01-06 14:56:58 +0900

    calc exam 11

---
+++ Class/calc/c/ex3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 1;
+
+int main() {
+    int i = 10;
+    printf("%d %d %d \n", i, i--, i--);
+
+    return 0;
+}
